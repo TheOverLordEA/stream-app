840331e35d4efcd144dd17732423346774090eb0
Erkhembayar Atarbold <74484951+TheOverLordEA@users.noreply.github.com>
2021-04-25 06:50:24 -0700

    a yarn style and login component was added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "rea";
+import Login from "./Compnents/Login";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <router>
+        <switch>
+          <route exact path="/">
+            <Login />
+          </route>
+        </switch>
+      </router>
+    </div>
+  );
 }
 
 export default App;
